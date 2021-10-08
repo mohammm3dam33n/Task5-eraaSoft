ce312d071871f543b5be829a110360376f1ef6aa
mohammm3dam33n <88665141+mohammm3dam33n@users.noreply.github.com>
2021-10-08 20:01:37 +0200

    Update main.php

--- mohammmed-ameen/main.php
+++ mohammmed-ameen/main.php
@@ -1,20 +1,28 @@
 <?php 
 
 
-// define a constant (title) and use it as the page <title>
-$title="PHP define() Function";
-define("URL",$title,true);
 
-	// create variables course name , price and description 
-  $course_Name="Back end develober";
+// define a constant (title) and use it as the page <title>
+$title="http://127.0.0.1/Task5%20(mohammed%20ammen)/mohammmed-ameen/";
+define("URL",$title,true);
+echo URL;
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+// create variables course name , price and description 
+ $course_Name="Back end develober";
   $price =5000;
   $description=nl2br("
-  1_Learn about HTML, URLs, and HTTP.
-  2_Learn about forms and how to handle inputs from your users.
-  3_Databases.
+  Learn about HTML, URLs, and HTTP.
+  Databases.
 ");
 
-	// echo the value and type of each of them
+// echo the value and type of each of them
+echo"// echo the value and type of each of them";
+echo "<br>";
+echo "<br>";
 echo"Course Name: ",$course_Name;//type string
 echo "<br>";
 var_dump($course_Name);//type String
@@ -28,56 +36,45 @@ echo "<br>";
 echo "Description: ", $description;
 echo "<br>";
 var_dump($description);//String
+
+
+echo "<br>";
+echo "<br>";
 echo "<br>";
 
-?>
-<p><?php echo URL;?></p>  
 
-
-
-<p>// “echo” true if $str1 or $str2 contains a whitespace after trimming them</p>
-<?php 
 // “echo” true if $str1 or $str2 contains a whitespace after trimming them
- $str1 = "  Does this string contain a whitespace ' ' ?    ";
+echo"// “echo” true if str1 or str2 contains a whitespace after trimming them";
+echo "<br>";
+$str1 = "  Does this string contain a whitespace ' ' ?    ";
  $str2 = "  What_about_this?  ";
- echo str_replace("  ","",$str1)
-?>
+ echo str_replace("  ","",$str1);
 
 
-<br>
-<br>
+
+ echo "<br>";
+ echo "<br>";
+ echo "<br>";
 
 
-<p>// “echo” true if $str1 or $str2 contains a whitespace after trimming them</p>
-<?php 
-// “echo” true if $str1 or $str2 contains a whitespace after trimming them
- $str1 = "  Does this string contain a whitespace ' ' ?    ";
- $str2 = "  What_about_this?  ";
- echo str_replace("  ","",$str1)
-?>
 
-
-<br>
-<br>
-<br>
-
-
-<p>// given 3 numbers echo  the absolute summation of them</p>
-<?php 
+echo"// given 3 numbers echo  the absolute summation of them";
+echo "<br>";
 // given 3 numbers echo  the absolute summation of them
   $n1 = 3;
   $n2 = -3;
   $n3 = -6;
   echo $n1+abs($n2)+abs($n3);
-?>
-
-<br>
-<br>
-<br>
+  
 
 
-<p>//given $str remove vowels from the given string</p>
- <?php 
+  echo "<br>";
+  echo "<br>"; 
+  echo "<br>"; 
+
+
+echo"//given str remove vowels from the given string";
+echo "<br>"; 
 //given $str remove vowels from the given string
  $str = "The vowels are 'aeiou', remove them from this string";
  echo str_replace(["a","e","i","o","u"],"",$str);
